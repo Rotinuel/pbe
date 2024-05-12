f2fec640eb743d7e1d403e27d8417e0a85587f6e
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2024-05-12 18:45:16 +0100

    footer again

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,12 @@
 export function Footer (){
     return(
         <>
-            <div className="mt-[12px] pt-[20px] pb-[20px] px-6 text-[#FFF] bg-[#0071c1]">
+            <div className="mt-[12px] pt-[20px] pb-[20px] px-6 text-[#FFF] bg-[#0071c1] lg:px-16 lg:font-bold lg:text-[48px]">
                 <div className="flex flex-col justify-start gap-x-[12px]">
-                    <h2 className="text-[16px] text-start">Contact Us</h2>
-                    <p className="text-[11px]">3, Savalou street, Wuse Zone 4, Abuja FCT, Nigeria.</p>
-                    <p className="text-[11px]">+234 (0) 806 812 9028, +234 (0) 706 643 3477</p>
-                    <p className="text-[11px]">info@premiumblueeconomy.com</p>
+                    <h2 className="text-[16px] text-start lg:text-[24px]">Contact Us</h2>
+                    <p className="text-[11px] lg:text-[20px]">3, Savalou street, Wuse Zone 4, Abuja FCT, Nigeria.</p>
+                    <p className="text-[11px] lg:text-[20px]">+234 (0) 806 812 9028, +234 (0) 706 643 3477</p>
+                    <p className="text-[11px] lg:text-[20px]">info@premiumblueeconomy.com</p>
                 </div>
                 <div className="text-[12px]">
 
